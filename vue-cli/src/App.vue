9caf01db61154b3a114a9d7d164eb81a4a26a6e9
MEK <ansenfrl@gmail.com>
2020-12-25 04:50:27 +0900

    viu-cli 연습

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <app-header v-bind:프롭스 속성이름='상위컴포넌트 데이터이름'></app-header> -->
+    <app-header 
+    v-bind:propsdata='str'
+    v-on:renew='renewStr'></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from './components/AppHeader.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+
+//이전에는 data:{ } 이렇게도 썼지만, .vue 에서는 function 으로해야됨 재사용성을위해
+data:function(){
+  return {
+    str: 'Header'
   }
+},
+components : {
+  'app-header': AppHeader
+},
+methods:{
+  renewStr:function(){
+    this.str='hi'
+  }
+}
+
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+<header>
+  <h1>{{propsdata}}</h1>
+  <button v-on:click='onclick'>send</button>  
+</header>
+</template>
+
+<script>
+export default {
+
+  props:['propsdata'],
+  methods:{
+    onclick:function(){
+      console.log('버튼클릭됨');
+      this.$emit('renew')
+    }
+  }
+
+
+}
+</script>
+
+<style>
+
+</style>

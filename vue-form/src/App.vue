543af6630d1e9c2b90e804c9124d551e5691efe6
MEK <ansenfrl@gmail.com>
2020-12-25 05:00:41 +0900

    폼 작성

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    hi
-  </div>
-</template>
+  <form action="">
+<div>
+  <label for="username">id: </label>
+  <input type="text" id="username">
+</div>
+<div>
+  <label for="password">pw: </label>
+  <input type="password" id="password">
+</div>
+<button>login</button>
+  </form> 
+  </template>
 
 <script>
 export default {
